424747b4cf0e60d121c8140d1467408355622687
Alex Ø. T. Hansen <94194707+System-Admins-ath@users.noreply.github.com>
2023-09-19 12:54:27 +0200

    Update DropPrimaryKeys.sql

--- src/mssql/query/dropprimarykeys/DropPrimaryKeys.sql
+++ src/mssql/query/dropprimarykeys/DropPrimaryKeys.sql
@@ -2,31 +2,33 @@ DECLARE @TableName NVARCHAR(128)
 DECLARE @ConstraintName NVARCHAR(128) 
 DECLARE primaryKeyCursor CURSOR FOR 
 SELECT
-OBJECT_NAME(object_id) AS TableName,
-name AS ConstraintName 
+    OBJECT_NAME(object_id) AS TableName,
+    name AS ConstraintName 
 FROM
-sys.indexes 
+    sys.indexes 
 WHERE
-is_primary_key = 1 
-AND OBJECTPROPERTY(object_id, 'IsUserTable') = 1 -- Exclude system tables
-OPEN primaryKeyCursor FETCH NEXT 
+    is_primary_key = 1 
+    AND OBJECTPROPERTY(object_id, 'IsUserTable') = 1 	-- Exclude system tables
+    OPEN primaryKeyCursor FETCH NEXT 
 FROM
-primaryKeyCursor INTO @TableName,
-@ConstraintName WHILE @@FETCH_STATUS = 0 
-BEGIN
-    DECLARE @DropPrimaryKeySQL NVARCHAR(MAX) 
+    primaryKeyCursor INTO @TableName,
+    @ConstraintName WHILE @@FETCH_STATUS = 0 
+    BEGIN
+        DECLARE @DropPrimaryKeySQL NVARCHAR(MAX) 
 SET
-@DropPrimaryKeySQL = 'ALTER TABLE [' + @TableName + '] DROP CONSTRAINT [' + @ConstraintName + ']' 
-BEGIN
-    TRY EXEC sp_executesql @DropPrimaryKeySQL 
-END
-TRY 
-BEGIN
-    CATCH
-END
-CATCH FETCH NEXT 
+    @DropPrimaryKeySQL = 'ALTER TABLE [' + @TableName + '] DROP CONSTRAINT [' + @ConstraintName + ']' 
+    BEGIN
+        TRY
+        PRINT @DropPrimaryKeySQL
+        EXEC sp_executesql @DropPrimaryKeySQL
+    END
+    TRY 
+    BEGIN
+        CATCH
+    END
+    CATCH FETCH NEXT 
 FROM
-primaryKeyCursor INTO @TableName,
-@ConstraintName 
-END
-CLOSE primaryKeyCursor DEALLOCATE primaryKeyCursor
+    primaryKeyCursor INTO @TableName,
+    @ConstraintName 
+    END
+    CLOSE primaryKeyCursor DEALLOCATE primaryKeyCursor
