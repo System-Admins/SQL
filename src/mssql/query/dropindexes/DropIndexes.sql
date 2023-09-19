dab7a253eb5e6a0bbb0494ed5781eae2b27997b6
Alex Ø. T. Hansen <94194707+System-Admins-ath@users.noreply.github.com>
2023-09-19 12:53:44 +0200

    Update DropIndexes.sql

--- src/mssql/query/dropindexes/DropIndexes.sql
+++ src/mssql/query/dropindexes/DropIndexes.sql
@@ -1,94 +1,139 @@
-DECLARE @TableName NVARCHAR(255) 
-DECLARE table_cursor CURSOR FOR 
-SELECT
-    QUOTENAME(s.name) + '.' + QUOTENAME(t.name) AS TableName 
-FROM
-    sys.tables t 
-    INNER JOIN
-        sys.schemas s 
-        ON t.schema_id = s.schema_id OPEN table_cursor FETCH NEXT 
-FROM
-    table_cursor INTO @TableName WHILE @@FETCH_STATUS = 0 
+DECLARE @table_name NVARCHAR(255)
+DECLARE @schema_name NVARCHAR(255)
+DECLARE @index_name NVARCHAR(255)
+DECLARE @index_type_desc NVARCHAR(60)
+DECLARE @constraint_name NVARCHAR(255)
+
+DECLARE @sql NVARCHAR(MAX)
+
+DECLARE table_cursor CURSOR FOR
+SELECT TABLE_SCHEMA, TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = 'BASE TABLE'
+
+OPEN table_cursor
+
+FETCH NEXT FROM table_cursor INTO @schema_name, @table_name
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT 'Table: ' + @schema_name + '.' + @table_name
+
+    DECLARE index_cursor CURSOR FOR
+    SELECT name, type_desc
+    FROM sys.indexes
+    WHERE object_id = OBJECT_ID(QUOTENAME(@schema_name) + '.' + QUOTENAME(@table_name)) AND is_primary_key = 0 AND is_unique_constraint = 0 AND type_desc <> 'CLUSTERED' AND type <> 4
+
+    OPEN index_cursor
+
+    FETCH NEXT FROM index_cursor INTO @index_name, @index_type_desc
+
+    WHILE @@FETCH_STATUS = 0
     BEGIN
-        BEGIN
-            TRY
-            DECLARE @ConstraintName NVARCHAR(255) 
-            DECLARE constraint_cursor CURSOR FOR 
-            SELECT
-            name 
-            FROM
-            sys.objects 
-            WHERE
-            parent_object_id = OBJECT_ID(@TableName) 
-            AND type = 'F' OPEN constraint_cursor FETCH NEXT 
-            FROM
-            constraint_cursor INTO @ConstraintName WHILE @@FETCH_STATUS = 0 
-            BEGIN
-                DECLARE @DisableConstraintSQL NVARCHAR(MAX) 
-            SET
-            @DisableConstraintSQL = 'ALTER TABLE ' + @TableName + ' NOCHECK CONSTRAINT ' + @ConstraintName EXEC sp_executesql @DisableConstraintSQL FETCH NEXT 
-            FROM
-            constraint_cursor INTO @ConstraintName 
-            END
-            CLOSE constraint_cursor DEALLOCATE constraint_cursor
-            DECLARE @IndexName NVARCHAR(255) 
-            DECLARE index_cursor CURSOR FOR 
-            SELECT
-                name 
-            FROM
-                sys.indexes 
-            WHERE
-                object_id = OBJECT_ID(@TableName) 
-                AND type = 2 OPEN index_cursor FETCH NEXT 
-            FROM
-                index_cursor INTO @IndexName WHILE @@FETCH_STATUS = 0 
-                BEGIN
-                    DECLARE @DropIndexSQL NVARCHAR(MAX) 
-            SET
-                @DropIndexSQL = 'DROP INDEX ' + @TableName + '.' + @IndexName EXEC sp_executesql @DropIndexSQL FETCH NEXT 
-            FROM
-                index_cursor INTO @IndexName 
-                END
-                CLOSE index_cursor DEALLOCATE index_cursor
-                DECLARE @ClusteredIndexName NVARCHAR(255) 
-                SELECT
-                    TOP 1 @ClusteredIndexName = name 
-                FROM
-                    sys.indexes 
-                WHERE
-                    object_id = OBJECT_ID(@TableName) 
-                    AND type = 1 IF @ClusteredIndexName IS NOT NULL 
-                    BEGIN
-                        DECLARE @DropClusteredIndexSQL NVARCHAR(MAX) 
-                SET
-                    @DropClusteredIndexSQL = 'DROP INDEX ' + @TableName + '.' + @ClusteredIndexName EXEC sp_executesql @DropClusteredIndexSQL 
-                    END
-                    -- Re-enable constraints
-                    DECLARE enable_constraint_cursor CURSOR FOR 
-                    SELECT
-                        name 
-                    FROM
-                        sys.objects 
-                    WHERE
-                        parent_object_id = OBJECT_ID(@TableName) 
-                        AND type = 'F' OPEN enable_constraint_cursor FETCH NEXT 
-                    FROM
-                        enable_constraint_cursor INTO @ConstraintName WHILE @@FETCH_STATUS = 0 
-                        BEGIN
-                        DECLARE @EnableConstraintSQL NVARCHAR(MAX) 
-                    SET
-                        @EnableConstraintSQL = 'ALTER TABLE ' + @TableName + ' CHECK CONSTRAINT ' + @ConstraintName EXEC sp_executesql @EnableConstraintSQL FETCH NEXT 
-                    FROM
-                        enable_constraint_cursor INTO @ConstraintName 
-                        END
-                        CLOSE enable_constraint_cursor DEALLOCATE enable_constraint_cursor 
-        END
-        TRY 
-        BEGIN
-            CATCH 
-        END
-        CATCH FETCH NEXT 
-                    FROM
-                        table_cursor INTO @TableName 
+        PRINT '- Dropping index: ' + @index_name
+        
+        SET @sql = 'DROP INDEX ' + QUOTENAME(@index_name) + ' ON ' + QUOTENAME(@schema_name) + '.' + QUOTENAME(@table_name)
+
+        BEGIN TRY
+            EXEC sp_executesql @sql
+            PRINT '- Index dropped successfully.'
+        END TRY
+        BEGIN CATCH
+            PRINT '- Error occurred while dropping index: ' + ERROR_MESSAGE()
+        END CATCH
+
+        FETCH NEXT FROM index_cursor INTO @index_name, @index_type_desc
     END
-    CLOSE table_cursor DEALLOCATE table_cursor
+
+    CLOSE index_cursor
+    DEALLOCATE index_cursor
+
+    -- Drop foreign key constraints that use clustered indexes
+    DECLARE fk_cursor CURSOR FOR
+    SELECT CONSTRAINT_NAME
+    FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' AND TABLE_SCHEMA = @schema_name AND TABLE_NAME = @table_name AND EXISTS (
+        SELECT 1
+        FROM sys.indexes
+        WHERE object_id = OBJECT_ID(QUOTENAME(@schema_name) + '.' + QUOTENAME(@table_name)) AND name = (
+            SELECT name
+            FROM sys.index_columns
+            WHERE object_id = sys.indexes.object_id AND column_id = sys.index_columns.column_id AND is_included_column = 0
+        ) AND type_desc = 'CLUSTERED'
+    )
+
+    OPEN fk_cursor
+
+    FETCH NEXT FROM fk_cursor INTO @constraint_name
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        PRINT '- Dropping foreign key constraint: ' + @constraint_name
+        
+        SET @sql = 'ALTER TABLE ' + QUOTENAME(@schema_name) + '.' + QUOTENAME(@table_name) + ' DROP CONSTRAINT ' + QUOTENAME(@constraint_name)
+
+        BEGIN TRY
+            EXEC sp_executesql @sql
+            PRINT '- Foreign key constraint dropped successfully.'
+        END TRY
+        BEGIN CATCH
+            PRINT '- Error occurred while dropping foreign key constraint: ' + ERROR_MESSAGE()
+        END CATCH
+
+        FETCH NEXT FROM fk_cursor INTO @constraint_name
+    END
+
+    CLOSE fk_cursor
+    DEALLOCATE fk_cursor
+
+    FETCH NEXT FROM table_cursor INTO @schema_name, @table_name
+END
+
+CLOSE table_cursor
+DEALLOCATE table_cursor
+
+BEGIN TRY
+    DECLARE @IndexName NVARCHAR(128)
+    DECLARE @SchemaName NVARCHAR(128)
+    DECLARE @TableName NVARCHAR(128)
+    DECLARE @DropStatement NVARCHAR(MAX)
+
+    DECLARE IndexCursor CURSOR FOR
+        SELECT 
+            SCHEMA_NAME(t.schema_id) AS SchemaName,
+            t.name AS TableName,
+            i.name AS IndexName
+        FROM 
+            sys.indexes i
+            INNER JOIN sys.tables t ON i.object_id = t.object_id
+        WHERE 
+            i.type_desc = 'CLUSTERED'
+        ORDER BY 
+            SchemaName,
+            TableName
+
+    OPEN IndexCursor
+
+    FETCH NEXT FROM IndexCursor INTO @SchemaName, @TableName, @IndexName
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @DropStatement = 'DROP INDEX ' + QUOTENAME(@IndexName) + ' ON ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName)
+        BEGIN TRY
+            PRINT @DropStatement
+			EXEC sp_executesql @DropStatement
+        END TRY
+        BEGIN CATCH
+            PRINT 'Error dropping index ' + QUOTENAME(@IndexName) + ' on table ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) + ': ' + ERROR_MESSAGE()
+        END CATCH
+        FETCH NEXT FROM IndexCursor INTO @SchemaName, @TableName, @IndexName
+    END
+
+    CLOSE IndexCursor
+    DEALLOCATE IndexCursor
+
+END TRY
+BEGIN CATCH
+    PRINT 'An error occurred while dropping the clustered indexes.'
+    PRINT ERROR_MESSAGE()
+END CATCH
